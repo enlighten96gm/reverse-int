0a59e7e689c43d262b888d35be509457785ae17c
enlighten96gm <gm.enlightenby@gmail.com>
2021-02-16 00:22:07 +0300

    feat: test complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let newArg = Math.abs(n)
+    let arr = newArg.toString()
+    let newArr = arr.split('').reverse().join('')
+    return Number(newArr)
 }
